35a2f271bb2e0f1807a20bfaa8496007bcc7e647
In-Wess-Tation <skolekonto318@gmail.com>
2023-04-14 13:27:41 +0200

    making search

---
+++ app/components/Search.jsx
@@ -0,0 +1,12 @@
+
+
+
+const Search = () => {
+    return ( 
+        <div className="w-full h-10 bg-grey text-grey rounded-3xl m-2">
+            <p>Search for movies, TV shows...</p>
+        </div>
+     );
+}
+ 
+export default Search;
