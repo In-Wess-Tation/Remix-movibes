bb4a39eb5a59b272528bcda9f04496cc15fef88e
In-Wess-Tation <skolekonto318@gmail.com>
2023-04-14 13:36:03 +0200

    removing a i tag

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
         <header className="bg-darkGrey w-full text-center leading-10 h-screen sticky top-0">
             <h1 className="text-teal text-4xl pb-5 pt-7">Movibes</h1>
             <div className="grid grid-rows-4 grid-cols-1">
-                <Link to="/" className="text-teal pb-16 pt-16 focus:bg-teal/50 focus:border-r-4 focus:border-teal"><i class="ri-home-line"></i>Home</Link>
+                <Link to="/" className="text-teal pb-16 pt-16 focus:bg-teal/50 focus:border-r-4 focus:border-teal">Home</Link>
                 <Link to="/tvSeries" className="text-teal pb-16 pt-16 focus:bg-teal/50 focus:border-r-4 focus:border-teal">TV Series</Link>
                 <Link to="/upComming" className="text-teal pb-16 pt-16 focus:bg-teal/50 focus:border-r-4 focus:border-teal">Upcomming</Link>
                 <Link to="/logOut" className="text-teal pb-16 pt-16 focus:bg-teal/50 focus:border-r-4 focus:border-teal">Log out</Link>
