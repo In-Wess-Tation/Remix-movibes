d28c4a6998f219b8926ac9bf81b834d5e6ab41bd
In-Wess-Tation <skolekonto318@gmail.com>
2023-04-17 08:44:07 +0200

    giving search an input

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -6,7 +6,7 @@ import "remixicon/fonts/remixicon.css";
 const Header = () => {
     return ( 
         <header className="bg-darkGrey w-full text-center leading-10 h-screen sticky top-0">
-            <h1 className="text-teal text-4xl pb-5 pt-7">Movibes</h1>
+            <h1 className="text-teal text-4xl pb-5 pt-7 font-bold">Movibes</h1>
             <div className="grid grid-rows-4 grid-cols-1">
                 <Link to="/" className="text-teal pb-16 pt-16 focus:bg-teal/50 focus:border-r-4 focus:border-teal">Home</Link>
                 <Link to="/tvSeries" className="text-teal pb-16 pt-16 focus:bg-teal/50 focus:border-r-4 focus:border-teal">TV Series</Link>
--- app/components/Search.jsx
+++ app/components/Search.jsx
@@ -4,7 +4,8 @@
 const Search = () => {
     return ( 
         <div className="w-full h-12 text-grey rounded-3xl mt-10 p-3 pl-8 font-bold bg-darkGrey">
-            <p>Search for movies, TV shows...</p>
+            <label htmlFor="search">Search for movies, TV shows...</label>
+            <input className="border-none bg-transparent" type="search" name="search" id="search" />
         </div>
      );
 }
