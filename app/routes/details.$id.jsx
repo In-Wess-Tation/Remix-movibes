ef95afda26c3ca43eac0a2645742d92ae77e520a
In-Wess-Tation <skolekonto318@gmail.com>
2023-04-18 12:36:04 +0200

    worked on details and log in page

--- app/routes/details.$id.jsx
+++ app/routes/details.$id.jsx
@@ -3,13 +3,13 @@ import { useLoaderData } from "@remix-run/react";
 import Header from "../components/Header";
 
 
-import movieCollage from "./svg'er/movieCollage.svg"
-import joker from "./svg'er/joker.svg"
-import topGun from "./svg'er/topGun.svg"
-import Adventure from "./svg'er/adventure.svg"
-import Drama from "./svg'er/drama.svg"
-import Popular from "./svg'er/popular.svg"
-import Fav from "./svg'er/fav.svg"
+import movieCollage from "../svg'er/movieCollage.svg"
+import joker from "../svg'er/joker.svg"
+import topGun from "../svg'er/topGun.svg"
+import Adventure from "../svg'er/adventure.svg"
+import Drama from "../svg'er/drama.svg"
+import Popular from "../svg'er/popular.svg"
+import Fav from "../svg'er/fav.svg"
 
 
 let imgPath ="https://image.tmdb.org/t/p/original"
@@ -41,36 +41,44 @@ return (
         <div><img className="rounded-3xl object-cover h-72 w-screen" src={imgPath + movies.poster_path} alt={movies.poster_path} /></div>
       </article>
 
-      <article className="flex">
+      <article className="flex gap-12">
 
         <article>
-        <div className="flex gap-5 text-2xl">
+        <div className="flex gap-5 text-2xl pb-5">
             <p>{movies.title}</p>
             <p>{movies.release_date}</p>
-            {movies.adult === false ? <p>PG-13</p> : movie.adult === true ? <p>PG-13 <BsDot className="dot"/></p> : null}
+            {movies.adult === false ? <p>PG-13</p> : movies.adult === true ? <p>PG-13 <BsDot className="dot"/></p> : null}
             <p>{movies.runtime}m</p>
-            <p className="text-grey border-2 border-grey rounded-3xl">Genre</p>
-            <p className="text-grey border-2 border-grey rounded-3xl">Genre</p>
+            <p className="text-grey border-2 border-grey rounded-3xl text-base pl-2 pr-2">Genre</p>
+            <p className="text-grey border-2 border-grey rounded-3xl text-base pl-2 pr-2">Genre</p>
         </div>
 
-        <div className="text-2xl">
+        <div className="text-xl ">
             <p>{movies.overview}</p>
         </div>
 
-        <div>
-
+        <div className="text-xl pt-5 leading-10">
+          <p className="border-b-2 border-t-2 border-grey pt-2 pb-2">Director: </p>
+          <p className="border-b-2 border-grey pt-2 pb-2">Writers: </p>
+          <p className="border-b-2 border-grey pt-2 pb-2">Stars: </p>
         </div>
         </article>
 
-        <article className="text-white">
+        <article className="text-white w-full">
         <div>
-            <img src={movieCollage} alt="movie collage" />
-            <img src={joker} alt="joker movie info" />
-            <img src={topGun} alt="top gun movie info" />
-            <img src={Adventure} alt="adventure genre" />
-            <img src={Drama} alt="drama genre" />
-            <img src={Popular} alt="popular movies" />
-            <img src={Fav} alt=" favorite movies" />
+          <div className="pr-12 text-xl">
+            <p className="text-right pb-5">{movies.vote_average}<span className="text-grey"> | 350k</span></p>
+            <button className="bg-teal w-full rounded-2xl p-3 text-center mb-5">See Showtimes</button>
+            <p className="text-center pb-5"> More watch options</p>
+          </div>
+
+            <img className="pb-5" src={movieCollage} alt="movie collage" />
+            <img className="pb-5" src={joker} alt="joker movie info" />
+            <img className="pb-5" src={topGun} alt="top gun movie info" />
+            <img className="pb-5" src={Adventure} alt="adventure genre" />
+            <img className="pb-5" src={Drama} alt="drama genre" />
+            <img className="pb-5" src={Popular} alt="popular movies" />
+            <img className="pb-5" src={Fav} alt=" favorite movies" />
         </div>
         </article>
 
--- app/routes/logOut.jsx
+++ app/routes/logOut.jsx
@@ -7,10 +7,20 @@ export const logOut = () => {
 
 export default function Index() {
   return (
-    <div className="flex">
+    <section className="flex">
+      <div>
       <Header />
-        <h1>Log out</h1>     
+      </div>
 
-    </div>
+      <article className="bg-dark text-white w-full flex flex-col justify-center pl-96 pr-96 leading-10">
+        <h1 className="text-center text-3xl pb-5">Log into your user here</h1>
+        <label htmlFor="userName">Username: </label>
+        <input type="text" name="userName" id="userName" />
+        <label htmlFor="email">Email: </label>
+        <input type="email" name="emaail" id="email" />
+        <label htmlFor="password">Password:</label>
+        <input type="password" name="password" id="password" />
+      </article>   
+    </section>
   );
 }
