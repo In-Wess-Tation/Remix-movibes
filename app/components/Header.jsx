4327529a555c424fb5e71d954642d3d5a57682bd
In-Wess-Tation <skolekonto318@gmail.com>
2023-04-20 09:21:30 +0200

    Link i header lavet om til NavLink

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,17 +1,20 @@
-import { Link } from "@remix-run/react";
+import { NavLink } from "@remix-run/react";
 import "remixicon/fonts/remixicon.css";
 
 
-
 const Header = () => {
+
+    const activeLink = "h-full bg-teal text-teal p-6 bg-opacity-50 flex items-center justify-center border-r-4 border-teal  font-semibold pr-10"
+    const normalLink = "h-full p-10 font-semibold m-auto text-grey pr-10"
+
     return ( 
         <header className="bg-darkGrey w-full text-center leading-10 h-screen sticky top-0">
             <h1 className="text-teal text-4xl pb-5 pt-7 font-bold">Movibes</h1>
             <div className="grid grid-rows-4 grid-cols-1">
-                <Link to="/" className="text-teal pb-16 pt-16 focus:bg-teal/50 focus:border-r-4 focus:border-teal">Home</Link>
-                <Link to="/tvSeries" className="text-teal pb-16 pt-16 focus:bg-teal/50 focus:border-r-4 focus:border-teal">TV Series</Link>
-                <Link to="/upComming" className="text-teal pb-16 pt-16 focus:bg-teal/50 focus:border-r-4 focus:border-teal">Upcomming</Link>
-                <Link to="/logOut" className="text-teal pb-16 pt-16 focus:bg-teal/50 focus:border-r-4 focus:border-teal">Log out</Link>
+                <NavLink to="/" className={({ isActive }) => (isActive ? activeLink : normalLink)}>Home</NavLink>
+                <NavLink to="/tvSeries" className={({ isActive }) => (isActive ? activeLink : normalLink)}>TV Series</NavLink>
+                <NavLink to="/upComming" className={({ isActive }) => (isActive ? activeLink : normalLink)}>Upcomming</NavLink>
+                <NavLink to="/logOut" className={({ isActive }) => (isActive ? activeLink : normalLink)}>Log out</NavLink>
             </div>
             
         </header>
